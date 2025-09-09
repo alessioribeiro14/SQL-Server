83877ec9f4a8a0b27ac106b6d44ff87e03c35cec
alessioribeiro14 <ribeiro.alessio07@gmail.com>
2025-09-09 13:28:56 -0300

    Add files via upload

---
+++ Minha primeira consulta.sql
@@ -0,0 +1,6 @@
+SELECT * FROM DimCustomer
+SELECT * FROM DimProduct
+SELECT ProductLabel, ProductName FROM DimProduct
+select Gender, Education from DimCustomer
+select * from DimGeography
+select GeographyType, RegionCountryName from DimGeography
