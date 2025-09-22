b794e2c29e2bb02a711bfccd306163ec76fc4a0b
alessioribeiro14 <ribeiro.alessio07@gmail.com>
2025-09-21 21:36:23 -0300

    Add files via upload

---
+++ SELECT DISTINCT.sql
@@ -0,0 +1,11 @@
+--retorna as informações da tabela de produtos
+select * from DimProduct
+
+--retorna sem repetiçôes a coluna de cores da tabela de produtos
+select distinct ColorName from Dimproduct 
+
+--retorna as informações da tabela de funcionários
+select * from DimEmployee
+
+--retorna de forma distinta os nomes dos departamentos
+select distinct DepartmentName from DimEmployee
